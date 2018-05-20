2176a8d7f33cf3001159c9846e605e7ebb9402d8
Milena Atanasova <atamasowa40@gmail.com>
2018-05-20 17:03:17 +0300

    Added all kinds of DFS implemented with generators.

---
+++ algorithms/tree-traversal/dfs.js
@@ -0,0 +1,97 @@
+const BinaryTreeNode = require('../../data-structures/trees/nodes/binary-tree-node');
+
+const binaryTree = new BinaryTreeNode(
+    5,
+    new BinaryTreeNode(
+        20,
+        new BinaryTreeNode(1, new BinaryTreeNode(23)),
+        new BinaryTreeNode(5),
+    ),
+    new BinaryTreeNode(
+        2,
+        new BinaryTreeNode(7),
+        new BinaryTreeNode(38),
+    ),
+);
+ 
+class DFS {
+    constructor(tree) {
+        this.tree = tree || null;
+    }
+
+    /**
+     * Root, Left, Right
+     */
+    static get preOrder() {
+        const order = function* (node) {
+            if (node === null) {
+                return;
+            }
+
+            yield node.value;
+
+            yield* order(node.leftChild);
+            yield* order(node.rightChild);
+        };
+
+        return order;
+    }
+
+    /**
+     * Left, Root, Right
+     */
+    static get inOrder() {
+        const order = function* (node) {
+            if (node === null) {
+                return;
+            }
+
+            yield* order(node.leftChild);
+
+            yield node.value;
+
+            yield* order(node.rightChild);
+        };
+
+        return order;
+    }
+
+    /**
+     * Left, Right, Root
+     */
+    static get postOrder() {
+        const order = function* (node) {
+            if (node === null) {
+                return;
+            }
+
+            yield* order(node.leftChild);
+            yield* order(node.rightChild);
+
+            yield node.value;
+        };
+
+        return order;
+    }
+
+    static print(generator) {
+        const result = [];
+        let generatedNodeValue = generator.next();
+
+        while (generatedNodeValue.value) {
+            result.push(generatedNodeValue.value);
+
+            generatedNodeValue = generator.next();
+        }
+
+        console.log('Orderd nodes-> ' + result.join(' '))
+    }
+}
+
+const preOrderedNodesGenerator = DFS.preOrder(binaryTree);
+const inOrderedNodesGenerator = DFS.inOrder(binaryTree);
+const postOrderedNodesGenerator = DFS.postOrder(binaryTree);
+
+DFS.print(preOrderedNodesGenerator);
+DFS.print(inOrderedNodesGenerator);
+DFS.print(postOrderedNodesGenerator);
--- data-structures/trees/nodes/binary-tree-node.js
+++ data-structures/trees/nodes/binary-tree-node.js
@@ -1,9 +1,25 @@
 class BinaryTreeNode {
     constructor(value, left, right) {
         this.value = value;
-        this.leftChild = left;
-        this.rightChild = right;
+        this.leftChild = left || null;
+        this.rightChild = right || null;
     }
 }
 
 module.exports = BinaryTreeNode;
+
+// static get postOrder() {
+//     // console.log(root.value);
+
+//     const order = (node) => {
+//         if (node === null) {
+//             return;
+//         }
+
+//         order(node.leftChild);
+//         order(node.rightChild);
+//         console.log(node.value);
+//     };
+
+//     return order;
+// }
